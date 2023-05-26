c27d19760f44ef14bb8566e7f8ab18c15fc6dc64
Karen Rugerio <karenrugerio@Karens-MacBook-Pro.local>
2023-05-26 11:41:01 -0600

    Added configurations :sparkles:

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from pandas import DataFrame
 import numpy as np
-from sklearn.metrics import classification_report
+#from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
 
 
 # atributosName=setInicial.columns[:-1]
@@ -11,47 +12,113 @@ from sklearn.neural_network import MLPClassifier
 # clasesName=setInicial[setInicial.columns[-1]].drop_duplicates()
 # print(atributosName)
 # print(clasesName)
+def one_hot_encoder(df, orig_column):
+  new_columns = df[orig_column].unique()
+  for name in new_columns:
+    df[f'{orig_column}_{name}'] = (df[orig_column] == name).astype(int)
+  df = df.drop(columns=[orig_column])
+  return df
+'''
+#Train 80% de datos
+X_train80 = pd.read_csv('./BalanceoRNA3/DiabetesFirst80.csv') 
+X_train80 = X_train80.drop_duplicates()
+Y_train80 = X_train80[' class']
 
-X_train80 = pd.read_csv('./BalanceoRNA3/DiabetesFirst80.csv') #Train 80% de datos
-Y_train80 = X_train80['class']
+#Y_train80 = 
+#Y_train80 = one_hot_encoder(Y_train80, 'class'))
+print(X_train80)
+print(Y_train80)
+X_train80 = X_train80.drop(columns=' class')
 
+#--------------------------
+#Test 20% de datos
 X_test20 = pd.read_csv('./BalanceoRNA3/DiabetesFirst20.csv') #Test 20% de datos
-Y_test20 = X_test20['class']
+X_test20 = X_test20.drop_duplicates()
+Y_test20 = X_test20[' class']
+X_test20 = X_test20.drop(columns=' class')
+'''
+#--------------------------
+#Fold 1 train
+X_trainK1 = pd.read_csv('./BalanceoRNA3/DiabetesK1Train.csv')
+X_trainK1 = X_trainK1.drop_duplicates()
+Y_trainK1 = X_trainK1[' class'] 
+X_trainK1 = X_trainK1.drop(columns=' class')
 
-X_trainK1 = pd.read_csv('./BalanceoRNA3/DiabetesK1Train.csv') #Fold 1 train
-Y_trainK1 = X_trainK1['class']
+    #--------------------------
+    #Fold 1 test
+X_testK1 = pd.read_csv('./BalanceoRNA3/DiabetesK1Test.csv')
+X_testK1 = X_testK1.drop_duplicates()
+Y_testK1 = X_testK1[[' class']]
+X_testK1 = X_testK1.drop(columns=' class')
+print(Y_trainK1)
+#print(Y_trainK1.columns)
+'''
+#--------------------------
+#Fold 2 train
+X_trainK2 = pd.read_csv('./BalanceoRNA3/DiabetesK1Train.csv') #Fold 1 train
+X_trainK2 = X_trainK2.drop_duplicates()
+Y_trainK2 = X_trainK2[' class']
+X_trainK2 = X_trainK2.drop(columns=' class')
 
-X_trainK2 = pd.read_csv('./BalanceoRNA3/DiabetesK1Test.csv') #Fold 1 train
-Y_trainK2 = X_trainK2['class']
+    #--------------------------
+    #Fold 2 test
+X_testK2 = pd.read_csv('./BalanceoRNA3/DiabetesK2Test.csv') #Fold 2 test
+X_testK2 = X_testK2.drop_duplicates()
+Y_testK2 = X_testK2[' class']
+X_testK2 = X_testK2.drop(columns=' class')
 
-X_testK2 = pd.read_csv('./BalanceoRNA3/DiabetesK2Train.csv') #Fold 2 test
-Y_testK2 = X_testK2['class']
-
+#--------------------------
+#Fold 3 train
 X_trainK3 = pd.read_csv('./BalanceoRNA3/DiabetesK3Train.csv') #Fold 3 train
-Y_trainK3 = X_trainK3['class']
-
+X_trainK3 = X_trainK3.drop_duplicates()
+Y_trainK3 = X_trainK3[' class']
+Y_trainK3 = X_trainK3.drop(columns=' class')
+    #--------------------------
+    #Fold 3 test
 X_testK3 = pd.read_csv('./BalanceoRNA3/DiabetesK3Test.csv') #Fold 3 test
-Y_testK3 = X_testK3['class']
-
+X_testK3 = X_testK3.drop_duplicates()
+Y_testK3 = X_testK3[' class']
+X_trainK3 = X_trainK3.drop(columns=' class')
+'''
 training_accuracy = [] #lista de aciertos de entrenamiento
 test_accuracy = [] #lista de aciertos de test
 training_error = [] #lista de errores de entrenamiento
 test_error = [] #lista de errores de test
 
-n_epoch = range(10, 200, 10) #granularidad de 10 a 200 de 10 en 10
+
+
+
+n_epoch = range(5, 200, 5) #granularidad de 10 a 200 de 10 en 10
 
 for epoch in n_epoch:
     # build the model
-    clasificador = MLPClassifier(solver='lbfgs', 
+    clasificador = MLPClassifier(solver='lbfgs', #metrica de calidad de resutlado
                     alpha=1e-5,
                     hidden_layer_sizes=(8), 
                     random_state=42,
-                    max_iter = epoch)
-    clasificador.fit(X_trainK1, Y_testK1)
+                    learning_rate_init= 0.2,
+                    momentum=0.2,
+                    activation = 'relu',
+                    max_iter=epoch)
+    clasificador.fit(X_trainK1, Y_trainK1)
     # record training set accuracy and error
-    training_accuracy.append(clasificador.score(X_trainK1, Y_testK1))
-    training_error.append(1.0 - clasificador.score(X_trainK1, Y_testK1))
+    training_accuracy.append(clasificador.score(X_trainK1, Y_trainK1))
+    training_error.append(1.0 - clasificador.score(X_trainK1, Y_trainK1))
     # record generalization accuracy and error
-    test_accuracy.append(clasificador.score(X_trainK1, Y_testK1))
-    test_error.append(1.0 - clasificador.score(X_trainK1, Y_testK1))
+    test_accuracy.append(clasificador.score(X_testK1, Y_testK1))
+    test_error.append(1.0 - clasificador.score(X_testK1, Y_testK1))
+    print("Accuracy on training set: {:.3f}".format(clasificador.score(X_trainK1, Y_trainK1)))
+    print("Accuracy on test set: {:.3f}".format(clasificador.score(X_testK1, Y_testK1)))
+plt.plot(n_epoch, training_accuracy, label="training accuracy")
+plt.plot(n_epoch, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_depth")
+plt.legend()
+plt.show()
 
+plt.plot(n_epoch, training_error, label="training error")
+plt.plot(n_epoch, test_error, label="test error")
+plt.ylabel("Error")
+plt.xlabel("n_depth")
+plt.legend()
+plt.show()
